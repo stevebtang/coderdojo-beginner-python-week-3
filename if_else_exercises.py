1bd33e8db20e44a889a82a537ba7c91d115335eb
Steve Tangsombatvisit <stang@netflix.comm>
2017-03-18 15:42:07 -0700

    initial commit

---
+++ if_else_exercises.py
@@ -0,0 +1,32 @@
+# If Else Exercises
+
+grocery_list = ["apples","bananas", "chicken"]
+
+# 1. Use the grocery_list variable defined above
+# a. Write an if/else statement so that if grocery_list has less than four items, print "we need more food".
+# b. if grocery_list has exactly four items, print "we have enough food"
+
+# 2. Modify your answer from question 1 so that if grocery_list has less than four items,  print "we need more food", AND ask the user to input a food item.
+# HINT : Remember your LIST exercises from last week.
+
+# 3. Modify your answer from question 1 & 2 so that if the grocery_list has MORE than six items, print "we have too much food"
+
+
+#############################################################
+# LOOP Exercises
+
+# 4. Create a loop that counts backwards 100 to 0 and print out all the numbers
+
+# 5. Complete the following bit of code
+
+new_grocery_list = []
+
+# a. Write a loop that asks user to input items into the new_grocery_list
+# b. When the new_grocery_list has exactly 5 items, stop the loop and print out the grocery list
+
+
+# BONUS. As the user is entering items in the list, print out how many more items user needs to add.
+# Example if the list has 0 items, print "you need to add five more items"
+
+# BONUS 2. After grocery_list has 5 items, check to make sure that "chicken" is one of the items. If not print out "you don't have chicken!"
+
